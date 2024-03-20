d7405b884210ee82aa19ffe98c9389c0c6a96537
mr-woodapple <39863485+mr-woodapple@users.noreply.github.com>
2024-03-20 13:42:03 +0100

    fixed the 23-6 timetables not loading

--- js/modules/jsonHandler.js
+++ js/modules/jsonHandler.js
@@ -1,7 +1,8 @@
-// Created 2023-11-16
-
-
-// load correct json, return json object
+/**
+ * Load the correct json file for the selected class.
+ * @param {*} selectedTimetable 
+ * @returns JSON object of the timetable.
+ */
 export function loadJSON(selectedTimetable) {
 
     // ideally automate this to get all files in the "timetables" folder and check if a matchin file is present
@@ -17,6 +18,22 @@ export function loadJSON(selectedTimetable) {
         case "it-23-3-B":
             var request = new XMLHttpRequest();
             request.open("GET", "../../timetables/C_IT23-3-B.json", false);
+            request.send(null);
+            var timetable = JSON.parse(request.responseText);
+
+            return timetable;
+
+        case "it-23-6-A":
+            var request = new XMLHttpRequest();
+            request.open("GET", "../../timetables/C_IT23-6-A.json", false);
+            request.send(null);
+            var timetable = JSON.parse(request.responseText);
+
+            return timetable;
+
+        case "it-23-6-B":
+            var request = new XMLHttpRequest();
+            request.open("GET", "../../timetables/C_IT23-6-B.json", false);
             request.send(null);
             var timetable = JSON.parse(request.responseText);
 
