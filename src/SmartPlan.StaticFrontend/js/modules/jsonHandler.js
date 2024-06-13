9496fe9c37436892e6f84a75ff76c4fb08d743d3
mr-woodapple <39863485+mr-woodapple@users.noreply.github.com>
2024-06-13 15:52:29 +0200

    updated navbar style, changed timetable folder

--- src/SmartPlan.StaticFrontend/js/modules/jsonHandler.js
+++ src/SmartPlan.StaticFrontend/js/modules/jsonHandler.js
@@ -5,11 +5,11 @@
  */
 export function loadJSON(selectedTimetable) {
 
-    // ideally automate this to get all files in the "timetables" folder and check if a matchin file is present
+    // ideally automate this to get all files in the "timetables" folder and check if a matching file is present
     switch (selectedTimetable) {
         case "it-23-3-A":
             var request = new XMLHttpRequest();
-            request.open("GET", "../../timetables/C_IT23-3-A.json", false);
+            request.open("GET", "./../SmartPlan.StaticFrontend/timetables/C_IT23-3-A.json", false);
             request.send(null);
             var timetable = JSON.parse(request.responseText);
 
@@ -17,7 +17,7 @@ export function loadJSON(selectedTimetable) {
 
         case "it-23-3-B":
             var request = new XMLHttpRequest();
-            request.open("GET", "../../timetables/C_IT23-3-B.json", false);
+            request.open("GET", "./../SmartPlan.StaticFrontend/timetables/C_IT23-3-B.json", false);
             request.send(null);
             var timetable = JSON.parse(request.responseText);
 
@@ -25,7 +25,7 @@ export function loadJSON(selectedTimetable) {
 
         case "it-23-6-A":
             var request = new XMLHttpRequest();
-            request.open("GET", "../../timetables/C_IT23-6-A.json", false);
+            request.open("GET", "./../SmartPlan.StaticFrontend/timetables/C_IT23-6-A.json", false);
             request.send(null);
             var timetable = JSON.parse(request.responseText);
 
@@ -33,7 +33,7 @@ export function loadJSON(selectedTimetable) {
 
         case "it-23-6-B":
             var request = new XMLHttpRequest();
-            request.open("GET", "../../timetables/C_IT23-6-B.json", false);
+            request.open("GET", "./../SmartPlan.StaticFrontend/timetables/C_IT23-6-B.json", false);
             request.send(null);
             var timetable = JSON.parse(request.responseText);
 
